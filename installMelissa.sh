177932573f71e097fff3bcf3cb6f9a04bc6125cf
cdrchops <standtrooper@gmail.com>
2016-12-22 13:26:44 -0600

    initial checkin of an installer needs to be completed

---
+++ installMelissa.sh
@@ -0,0 +1,19 @@
+#apt-get update
+#apt-get upgrade
+#apt-get install git
+
+
+#wget https://launchpad.net/python-weather-api/trunk/0.3.8/+download/pywapi-0.3.8.tar.gz
+#tar -zxvf pywapi-0.3.8.tar.gz
+#python pywapi-0.3.8/setup.py build
+#python pywapi-0.3.8/setup.py install
+
+#sudo apt-get install libsdl2-dev libfreetype6-dev python-pygame python-dev
+
+wget http://mpg123.de/download/mpg123-1.23.8.tar.bz2
+tar -jxvf mpg123-1.23.8.tar.bz2
+cd mpg123-1.23.8
+./configure
+make && make install
+
+#pip install -r requirements.txt
